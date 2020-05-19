2cce4f8585adc4ce9a387e02b8011b4e12605f4e
Belayet Hossain <2020belayethossain@gmail.com>
2020-05-19 22:17:02 +0600

    modify code

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
-    const transfromIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transfromIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((k, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />
         })
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
 
-    console.log(transfromIngredients);
+    if (transfromIngredients.length === 0) {
+        transfromIngredients = <p>Please start adding ingredients</p>
+    }
     
   return (
     <div className={classes.Burger}>
